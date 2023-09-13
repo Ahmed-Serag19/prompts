e5ff6d6580218950edbc7d9e5a76a282143c8fe7
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2023-09-13 23:09:10 +0300

    Fixed image issue

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -51,10 +51,10 @@ const PromptCard = ({
 
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post?.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {post?.creator?.email}
             </p>
           </div>
         </div>
@@ -83,7 +83,7 @@ const PromptCard = ({
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id &&
+      {session?.user.id === post?.creator?._id &&
         pathName === '/profile' && (
           <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
             <p
